3ef76c45902c79d471fb0aff9e33ad027202ea9e
Ben Daws <ben@bendaws.net>
2024-10-18 16:10:54 +0000

    plugins: open, save, and quit fns

--- src/plugins/microvim_api.lua
+++ src/plugins/microvim_api.lua
@@ -14,3 +14,21 @@ end
 function microvim_api.query(text)
     return microvim_core_api.vimprompt(text)
 end
+
+function microvim_api.open(filename)
+    microvim_core_api.openFile(filename)
+end
+
+function microvim_api.save()
+    microvim_core_api.saveFile()
+end
+
+function microvim_api.exit()
+    microvim_core_api.quitMicrovim()
+end
+
+function microvim_api.quit()
+    microvim_core_api.quitMicrovim()
+end
+
+return microvim_api
--- src/plugins/microvim_core_api.h
+++ src/plugins/microvim_core_api.h
@@ -21,9 +21,45 @@ void vimprompt(lua_State *L) {
     lua_pushstring(L, editorPrompt(promptMessageText, blank_fallback));
 }
 
+void quitMicrovim(lua_State *L) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    exit(0);
+}
+
+void openFile(lua_State *L) {
+    char* filepath = lua_tostring(L, 1);
+    editorOpen(filepath);
+}
+
+void saveFile(lua_State *L) {
+    editorSave();
+}
+
 int luaopen_microvim(lua_State *L) {
     lua_pushcfunction(L, vimstatus);
     lua_pushcfunction(L, vimprompt);
 
+    lua_pushcfunction(L, quitMicrovim);
+    lua_pushcfunction(L, openFile);
+    lua_pushcfunction(L, saveFile);
+
     return 1;
+}
+
+void load_plugin(char* script_path)
+{
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	if(luaL_loadfile(L, script_path))
+		err_exit(L, "luaL_loadfile failed.\n");
+
+	if(lua_pcall(L, 0, 0, 0))
+		editorSetStatusMessage(("plugin \"%s\" failed to load", script_path));
+
+	lua_close(L);
+
+	return 0;
 }
