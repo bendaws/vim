547ac4069171b8b9f0faf8fa491f2148210d3dde
Ben Daws <ben@bendaws.net>
2024-10-21 14:26:42 +0000

    plugins: import lua5.4

--- docs/index.md
+++
@@ -1,5 +0,0 @@
-# MicroVim Documentation
-This page contains documentation for MicroVim. It is mostly planned features, it's core features are easy to understand.
-
-## Lua Plugins
-**Goal:**: Add support for building *basic* plugins in Lua 5.1.
---
+++ lua/examples/command_bar.lua
@@ -0,0 +1,30 @@
+local command_bar = {}
+local vim2 = require("vim2")
+
+command_bar.version = "1.0.0"
+command_bar.name = "Command Bar"
+command_bar.author = "John Doe"
+
+function helpCmd(args)
+    print("help was ran")
+end
+
+local commands = {
+    {
+        "help",
+        helpCmd,
+    }
+}
+
+function onCommandBar()
+    local command_raw = vim2.question(":") -- will emulate the :
+    local command = command_raw.split(" ")
+
+    for _, commandTemplate in commands do
+        if commandTemplate[1] == command[1] then
+            commandTemplate[2]()
+        end    
+    end
+end
+
+vim2.set_keybind(":", onCommandBar)
